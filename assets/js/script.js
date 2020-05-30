b7cad3c7e35d7314b4182e05043d866a08846c37
Raffay Ahmed <raf.amed97@gmail.com>
2020-05-29 20:11:19 -0700

    updated script file and ReadMe

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,95 +1,275 @@
-// Assignment Code
-//Button
+//Assigns Buttons and Check Boxes to Variables
 var generateBtn = document.querySelector("#generate");
-//Check Boxes
-var upperCheck = document.querySelector("#uppercases");
-var lowerCheck = document.querySelector("#lowercases");
-var numCheck = document.querySelector("#numbers");
-var specialCheck = document.querySelector("#specialchar");
-var lengthForm = document.querySelector("#length");
+var uppercases = document.querySelector("#uppercases");
+var lowercases = document.querySelector("#lowercases");
+var numbers = document.querySelector("#numbers");
+var specialchar = document.querySelector("#specialchar");
+var length = document.querySelector("#length")
 
-//Functions To Generate Random
-function randomlower() {
-  return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
-}
-function randomupper() {
-  return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
-}
-function randomnumber() {
-  const numbers = "0123456789";
-  return numbers[Math.floor(Math.random() * numbers.length)];
-}
-function randomspecial() {
-  const specialChar = "._-~[]{}(),:;?^$#!\\/+%@";
-  return specialChar[Math.floor(Math.random() * specialChar.length)];
+//Functions to Genereate Random Characters
+function generateupper(){
+  var upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+  return upper[Math.floor(Math.random() * upper.length)]
 }
 
-//Object to call on to trigger functions for password generation
-const randomizer = {
-  lower: randomlower,
-  upper: randomupper,
-  number: randomnumber,
-  special: randomspecial,
-};
+function generatelower(){
+  var lower = "abcdefghijklmnopqrstuvwxyz"
+  return lower[Math.floor(Math.random() * lower.length)]
+}
 
-//Takes User Input and then Creates Password
+function generatenumber(){
+  var number = "0123456789"
+  return number[Math.floor(Math.random() * number.length)]
+}
+
+function generatespecial(){
+  var special = "._-~[]{}(),:;?^$#!\\/+%@"
+  return special[Math.floor(Math.random() * special.length)]
+}
+
+
+//Functions to Create Random Password Strings
+function oneChoice(up, low, num, spec, len){
+  var pass = ''
+  if(up == true){
+    for(i = 0; i < len; i++){
+      pass += generateupper()
+    }
+  }
+  if(low == true){
+    for(i = 0; i < len; i++){
+      pass += generatelower()
+    }
+  }
+  if(num == true){
+    for(i = 0; i < len; i++){
+      pass += generatenumber()
+    }
+  }
+  if(spec == true){
+    for(i = 0; i < len; i++){
+      pass += generatespecial()
+    }
+  }
+  return pass;
+}
+function twoChoices(up, low, num, spec, len){
+  var pass = ''
+  var choice = ''
+  if(num == true && up == true){
+    for(i = 0; i < len; i++){
+      choice = Math.floor(Math.random() * 2)
+      console.log(choice)
+      if(choice === 0){
+        pass += generateupper()
+      }
+      if(choice === 1){
+        pass += generatenumber()
+      }
+    }
+  }
+  if(num == true && low == true){
+    for(i = 0; i < len; i++){
+      choice = Math.floor(Math.random() * 2)
+      console.log(choice)
+      if(choice === 0){
+        pass += generatelower()
+      }
+      if(choice === 1){
+        pass += generatenumber()
+      }
+    }
+  }
+  if(num == true && spec == true){
+    for(i = 0; i < len; i++){
+      choice = Math.floor(Math.random() * 2)
+      console.log(choice)
+      if(choice === 0){
+        pass += generatespecial()
+      }
+      if(choice === 1){
+        pass += generatenumber()
+      }
+    }
+  }
+  if(low == true && up == true){
+    for(i = 0; i < len; i++){
+      choice = Math.floor(Math.random() * 2)
+      console.log(choice)
+      if(choice === 0){
+        pass += generateupper()
+      }
+      if(choice === 1){
+        pass += generatelower()
+      }
+    }
+  }
+  if(spec == true && up == true){
+    for(i = 0; i < len; i++){
+      choice = Math.floor(Math.random() * 2)
+      console.log(choice)
+      if(choice === 0){
+        pass += generateupper()
+      }
+      if(choice === 1){
+        pass += generatespecial()
+      }
+    }
+  }
+  if(low == true && spec == true){
+    for(i = 0; i < len; i++){
+      choice = Math.floor(Math.random() * 2)
+      console.log(choice)
+      if(choice === 0){
+        pass += generatelower()
+      }
+      if(choice === 1){
+        pass += generatespecial()
+      }
+    }
+  }
+
+  return pass;
+}
+function threeChoices(up, low, num, spec, len){
+  var pass = ''
+  var choice = ''
+
+  if(num === true && up === true && low === true){
+    for(i = 0; i < len; i++){
+      choice = Math.floor(Math.random() * 3)
+      switch(choice){
+        case 0:
+          pass += generatenumber()
+          break;
+        case 1:
+          pass += generateupper()
+          break;
+        case 2:
+          pass += generatelower()
+          break;
+      }
+    }
+  }
+  if(num === true && up === true && spec === true){
+    for(i = 0; i < len; i++){
+      choice = Math.floor(Math.random() * 3)
+      switch(choice){
+        case 0:
+          pass += generatenumber()
+          break;
+        case 1:
+          pass += generateupper()
+          break;
+        case 2:
+          pass += generatespecial()
+          break;
+      }
+    }
+  }
+  if(num === true && spec === true && low === true){
+    for(i = 0; i < len; i++){
+      choice = Math.floor(Math.random() * 3)
+      switch(choice){
+        case 0:
+          pass += generatenumber()
+          break;
+        case 1:
+          pass += generateupper()
+          break;
+        case 2:
+          pass += generatelower()
+          break;
+      }
+    }
+  }
+  if(spec === true && up === true && low === true){
+    for(i = 0; i < len; i++){
+      choice = Math.floor(Math.random() * 3)
+      switch(choice){
+        case 0:
+          pass += generatenumber()
+          break;
+        case 1:
+          pass += generateupper()
+          break;
+        case 2:
+          pass += generatelower()
+          break;
+      }
+    }
+  }
+  return pass
+}
+function fourChoices(len){
+  var pass = ''
+  var choice = ''
+
+  for(i = 0; i < len; i++){
+    choice = Math.floor(Math.random() * 4)
+    switch(choice){
+      case 0:
+        pass += generatespecial()
+        break;
+      case 1:
+        pass += generatenumber()
+        break;
+      case 2:
+        pass += generatelower()
+        break;
+      case 3:
+        pass += generateupper()
+        break;
+    }
+  }
+
+  return pass
+}
+
+//Function to Create Random Password
+function generatepassword(){
+  var up = uppercases.checked;
+  var low = lowercases.checked;
+  var num = numbers.checked;
+  var spec = specialchar.checked;
+  var len = parseInt(length.value, 10)
+
+  typesChosen = up + low + num + spec
+  
+  let password = ''
+
+  if(typesChosen === 0){return "Please make choices"}
+  if(len < 4){return "Please choose length between 4 - 128"}
+  if(len > 128){return "Please choose length between 4 - 128"}
+
+  switch(typesChosen){
+    case 1:
+      console.log(1)
+      password = oneChoice(up, low, num, spec, len)
+      break;
+    case 2:
+      console.log(2)
+      password = twoChoices(up, low, num, spec, len)
+      break;
+    case 3:
+      console.log(3)
+      password = threeChoices(up, low, num, spec, len)
+      break;
+    case 4:
+      password = fourChoices(len)
+      console.log(4)
+      break;
+  }
+
+  return password
+}
+
+//Function to Trigger Password Generation and Push to Webpage
 function writePassword() {
-  //Reads the Selection of the Password Form/Checks
-  const length = parseInt(lengthForm.value, 10);
-  const chklower = lowerCheck.checked;
-  const chkupper = upperCheck.checked;
-  const chknum = numCheck.checked;
-  const chkspecial = specialCheck.checked;
-
-  //References the Generate Password Function
-  var password = generatePassword(
-    length,
-    chklower,
-    chkupper,
-    chknum,
-    chkspecial
-  );
-
-  //Initializes variable for password textfield replacement
+  var password = generatepassword();
   var passwordText = document.querySelector("#password");
-
-  //Sets Text Form to Generated Password
+  
   passwordText.value = password;
 }
 
-//Password Generator
-function generatePassword(length, lower, upper, number, special) {
-  //Initiates blank password string
-  let pass = "";
-
-  //Checks the amount of selections
-  const diffTypes = lower + upper + number + special;
-  console.log(diffTypes);
-  //Creates Object of Selections and Filters out which one is not selected
-  const arrayTypes = [{ lower }, { upper }, { number }, { special }];
-  //Filters the values that are not true
-  const filterTypes = arrayTypes.filter((item) => Object.values(item)[0]);
-  console.log(arrayTypes);
-  //Lets the user know to make a selection if nothing is chosen
-  if (diffTypes === 0) {
-    return "Please make selections";
-  }
-  if(length === 0 || length < 8 || length > 128)
-  {
-    return "Please choose a length from 8 - 128 characters";
-  }
-  //Keeps adding a character based on the length chosen
-  for (i = 0; i < length; i += diffTypes) {
-    filterTypes.forEach((type) => {
-      const gener = Object.keys(type)[0];
-
-      pass += randomizer[gener]();
-    });
-  }
-  return pass;
-
-
-}
-
-// Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
